41a3395f490210afbbb114f87c6f0b5f00f94c7a
Gyuwon Yi <gyuwon@live.com>
2016-04-22 23:08:46 +0900

    Application Insights 예외 추적을 설정합니다.

--- ContactManager/App_Start/FilterConfig.cs
+++ ContactManager/App_Start/FilterConfig.cs
@@ -1,5 +1,5 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using ContactManager.Filters;
 
 namespace ContactManager
 {
@@ -7,7 +7,7 @@ namespace ContactManager
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ApplicationInsightsHandleErrorAttribute());
         }
     }
 }
---
+++ ContactManager/Filters/ApplicationInsightsHandleErrorAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+
+namespace ContactManager.Filters
+{
+    public class ApplicationInsightsHandleErrorAttribute : HandleErrorAttribute
+    {
+        private static TelemetryClient _telemetryClient;
+
+        public static TelemetryClient TelemetryClient
+        {
+            get
+            {
+                if (_telemetryClient == null)
+                    _telemetryClient = new TelemetryClient();
+
+                return _telemetryClient;
+            }
+        }
+
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext?.Exception != null)
+                TelemetryClient.TrackException(filterContext.Exception);
+
+            base.OnException(filterContext);
+        }
+    }
+}
