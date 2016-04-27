bacff2a7232ec317120c19b7ef44c3bf707c7f4e
Gyuwon Yi <gyuwon@live.com>
2016-04-28 01:16:50 +0900

    ContactsRespondsOK 테스트 케이스를 추가합니다.

---
+++ ContactManager.Tests.Functional/AcceptanceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContactManager.Tests.Functional
+{
+    [TestClass]
+    public class AcceptanceTest
+    {
+        [TestMethod]
+        public async Task ContactsRespondsOK()
+        {
+            string appHost = "https://contactmanager-app.azurewebsites.net";
+            string path = "/contacts";
+            var uri = new Uri(new Uri(appHost), path);
+            var httpClient = new HttpClient();
+
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
