2276ff560e7b7649f5b76671b6c67780e618ea2c
Gyuwon Yi <gyuwon@live.com>
2016-04-28 09:57:24 +0900

    테스트 그룹을 Application Insights 측정 속성에 추가합니다.

---
+++ ContactManager/Telemetry/TestGroupTelemetryInitializer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using static System.Configuration.ConfigurationManager;
+
+namespace ContactManager.Telemetry
+{
+    public class TestGroupTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            string testGroup = AppSettings["TestGroup"] ?? "A";
+            telemetry.Context.Properties["Test Group"] = testGroup;
+        }
+    }
+}
